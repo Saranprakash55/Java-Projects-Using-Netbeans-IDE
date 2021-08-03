b3fef37286112e14e9a2c8af8563db0d378ea981
Saranprakash55 <87431094+Saranprakash55@users.noreply.github.com>
2021-08-03 11:21:59 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,18 @@
+package leap.year;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+       int n = 2015;
+       
+       if (n % 4 == 0)
+       {
+           System.out.println("Leap Year");
+       }
+       else 
+       {
+           System.out.println("Not a Leap Year");
+       }
+    }
+    
+}
